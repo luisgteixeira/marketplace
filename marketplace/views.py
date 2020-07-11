434f0ef79f3fce8bad2ec66208fef34d9cdcd990
Luís Guilherme <luisteixeiira@gmail.com>
2020-07-11 18:50:54 -0300

    Busca registro por email ou id_pessoa

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,12 +1,62 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 from marketplace.models import Pessoa
-from rest_framework import viewsets
 from marketplace.serializers import PessoaSerializer
 
 
-class PessoaViewSet(viewsets.ModelViewSet):
+@csrf_exempt
+def pessoas(request):
     """
-    endpoint da API que permite usuário visualizar ou editar.
+    Busca todas as pessoas cadastradas, ou cria uma nova pessoa
     """
-    queryset = Pessoa.objects.all()
-    serializer_class = PessoaSerializer
-    print(serializer_class)
+    if request.method == 'GET':
+        pessoas = Pessoa.objects.all()
+        serializer = PessoaSerializer(pessoas, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = PessoaSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def pessoa_id(request, id_pessoa):
+    """
+    Busca ou deleta pessoa através do id_pessoa
+    """
+    try:
+        pessoa = Pessoa.objects.filter(id_pessoa=id_pessoa)
+    except Pessoa.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = PessoaSerializer(pessoa, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'DELETE':
+        pessoa.delete()
+        return HttpResponse(status=204)
+
+
+@csrf_exempt
+def pessoa_email(request, email):
+    """
+    Busca ou deleta pessoa através do email
+    """
+    try:
+        pessoa = Pessoa.objects.filter(email=email)
+    except Pessoa.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = PessoaSerializer(pessoa, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'DELETE':
+        pessoa.delete()
+        return HttpResponse(status=204)
